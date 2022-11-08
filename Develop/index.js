e7d7830a3c589f7021fbfaf58c01ddd534db9aff
a1exlin <104739337+a1exlin@users.noreply.github.com>
2022-11-07 20:02:21 -0500

    updated files

--- Develop/index.js
+++ Develop/index.js
@@ -1,45 +1,45 @@
 // run node index.js here
 const inquirer = require('inquirer');
+const template = require('../sources/template');
 const fs = require('fs');
 const generate = require('../utils/generate.js');
-const { Engineers, EngineerQ } = require('../assets/Engineer.js');
+const {Manager, ManagerQuestions} = require('../assets/Manager');
+const {Engineer, EngineerQuestions} = require('../assets/Engineer');
+const {Intern, InterQuestions} = require('../assets/Intern');
 
-const {Manager, ManagerQuestions} = require('./assets/Manager.js');
-const {Engineer, EngineerQuestions} = require('./assets/Engineer');
-const {Intern, InterQuestions} = require('./assets/Intern');
-const { init } = require('../../Project-2/models/User.js');
 
 // code below will be sent into this Employee array when code is running on node.js
 const Employees = [];
 
 const display = () => {
-	managerQ
+	managerQ();
 }
 const managerQ =() => {
 	inquirer.prompt(ManagerQuestions)
 
 	.then((answers) =>{
-		answer = new Manager(answer.name, answers.id, answers.email, answers.officeNum)
+		let manager = new Manager(answers.name, answers.id, answers.email, answers.officeNum)
+		Employees.push(manager);
 		return EmployeesPrompt();
 	})
 
 }
 
-const engineerQ =() => {
-	inquirer.promt(EngineerQuestions) 
+const EngineerQ =() => {
+	inquirer.prompt(EngineerQuestions) 
 	.then((answers) => {
-		answers = new Engineers(answer.name, answer.id, answers.github);
-		Employees.push(answers);
-		return EmployeePrompts();
+		let engineer = new Engineer(answers.name, answers.id, answers.github);
+		Employees.push(engineer);
+		return EmployeesPrompt();
 	})
 
 }
 const InternQ =() => {
 	inquirer.prompt(InterQuestions)
 	.then((answers) => {
-		answers = new Intern(answers.name, answers.id, answers.email, answers.school)
+		let intern = new Intern(answers.name, answers.id, answers.email, answers.school)
 
-		display.push(answers);
+		Employees.push(intern);
 		return EmployeesPrompt();
 	})
 }
@@ -52,11 +52,13 @@ const EmployeesPrompt = () => {
 		choices: [
 			{name: 'Engineer', value:  'addEngineer'},
 			{name: 'Intern', value: 'addIntern'},
+			{name: 'finished', value: 'finished'},
 		]
 	}])
+
 	.then(answer => {
-		if(answer.employeeType === 'addEngineer') { engineerQ();};
-		if(answer.employeeType === 'addintern') { internQ();};
+		if(answer.employeeType === 'addEngineer') { EngineerQ();};
+		if(answer.employeeType === 'addintern') { InternQ();};
 		if(answer.employeeType === 'finished')  {
 			let html = template(Employees)
 			console.log('...');
